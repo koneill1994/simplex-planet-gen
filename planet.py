5472594344b0edaadedd737711ba58a476ce126a
Kevin O'Neill <koneill1994@gmail.com>
2017-01-18 15:25:26 -0500

    Runtime generated black mask

--- planet.py
+++ planet.py
@@ -1,7 +1,7 @@
 # planet.py
 # K. O'Neill
 
-import sys, os, pygame, math, PIL, noise
+import sys, os, pygame, math, PIL, noise, time
 from PIL import Image
 from noise import snoise2
 
@@ -23,10 +23,10 @@ green = 0,255,0,255
 yellow = 255,255,0,255
 
 screen = pygame.display.set_mode(size)
-
+'''
 mask = pygame.image.load("mask.png")
 maskrect=mask.get_rect()
-
+'''
 maps=[]
 
 
@@ -38,11 +38,49 @@ maps=[]
 
 # string = im.convert("RGBA").tostring("raw", "RGBA")
 
+# to do clouds make sure to use 
+# convert_alpha(Surface) -> Surface
+
+#https://stackoverflow.com/questions/16880128/pygame-is-there-any-way-to-only-blit-or-update-in-a-mask
+
 
 # functions go here
 
 #planetsize = w, h = 128, 128
 planetsize = w, h = 400, 400
+
+
+
+def sqrdistance(a,b):
+  return (a[0]-b[0])**2 + (a[1]-b[1])**2
+
+def createmask(width,height):
+  start_time = time.clock()
+  im = Image.new( 'RGBA', (width,height), (0,0,0,0)) # create a new blank image
+  r = min(width/2, height/2)
+  center=(int(width/2),int(height/2))
+  pix = im.load()
+  for x in range(width):
+    for y in range(height):
+      sqrdist=sqrdistance((x,y),center)
+      if sqrdist < r**2:
+        pix[x, y] = (256,256,256,256)
+  print "mask created,", time.clock() - start_time, "SECONDS"
+  return im.convert("RGBA").tobytes("raw", "RGBA")
+
+def createdarkmask(width,height):
+  start_time = time.clock()
+  im = Image.new( 'RGBA', (width,height), (0,0,0,256)) # create a new blank image
+  r = min(width/2, height/2)
+  center=(int(width/2),int(height/2))
+  pix = im.load()
+  for x in range(width):
+    for y in range(height):
+      sqrdist=sqrdistance((x,y),center)
+      if sqrdist < r**2:
+        pix[x, y] = (0,0,0,0)
+  print "mask created,", time.clock() - start_time, "SECONDS"
+  return im.convert("RGBA").tobytes("raw", "RGBA")
 
 #TESTING
 def Ice(height, y):
@@ -77,6 +115,7 @@ def GradientColor(height):
   return (int(256*height),int(256*height),int(256*height),256)
 
 def PerlinNoise(width, height, xoff, yoff, scale=.01, octaves=2, persistence=.2, lacunarity=3):
+  start_time = time.clock()
   im = Image.new( 'RGBA', (width*3,height), "black") # create a new black image
   pix = im.load()
   for x in range(width*3):
@@ -84,16 +123,19 @@ def PerlinNoise(width, height, xoff, yoff, scale=.01, octaves=2, persistence=.2,
       pix[x, y] = PlanetColor(snoise2((x+xoff)*scale,(y+yoff)*scale,octaves, persistence, lacunarity,width*scale),(x,y))
       #noise2(x, y, octaves=1, persistence=0.5, lacunarity=2.0, repeatx=1024, repeaty=1024, base=0.0)
       #print snoise2(x+xoff,y+yoff,1, 0.6,5,width)
+  print "Terrain created,", time.clock() - start_time, "SECONDS"
   return im.convert("RGBA").tobytes("raw", "RGBA")
   
 def PerlinNoise_Raw(width, height, xoff, yoff, scale=.01, octaves=2, persistence=.2, lacunarity=3):
+  start_time = time.clock()
   im = Image.new( 'RGBA', (width*3,height), "black") # create a new black image
   pix = im.load()
   for x in range(width*3):
     for y in range(height):
-      pix[x, y] = GradientColor(snoise2((x+xoff)*scale,(y+yoff)*scale,octaves, persistence, lacunarity,width*scale)/2+1)
+      pix[x, y] = GradientColor(snoise2((x+xoff)*scale,(y+yoff)*scale,octaves, persistence, lacunarity,width*scale)/2+1/2)
       #noise2(x, y, octaves=1, persistence=0.5, lacunarity=2.0, repeatx=1024, repeaty=1024, base=0.0)
       #print snoise2(x+xoff,y+yoff,1, 0.6,5,width)
+  print "Perlin created,", time.clock() - start_time, "SECONDS"
   return im.convert("RGBA").tobytes("raw", "RGBA")
   
 
@@ -117,9 +159,23 @@ def create_map(surface):
   maps.append([surface,rect])
 
 
+
+# this will create the different maps for the planet
 create_map(pygame.image.fromstring(PerlinNoise(w,h,0,0), (w*3,h), 'RGBA'))
 create_map(pygame.image.fromstring(PerlinNoise_Raw(w,h,0,0), (w*3,h), 'RGBA'))
 create_map(pygame.image.fromstring(TestGradient(w,h,0,0),(w*3,h), 'RGBA'))
+
+
+# this creates the black mask that goes over & makes it a circle
+mask = pygame.image.fromstring(createdarkmask(width,height),(width,height), 'RGBA').convert_alpha()
+maskrect=mask.get_rect()
+
+# transparency mask dummied out code
+#https://stackoverflow.com/questions/16880128/pygame-is-there-any-way-to-only-blit-or-update-in-a-mask
+'''
+mask_new = pygame.image.fromstring(createmask(width,height),(width,height), 'RGBA').convert_alpha()
+mask_new_rect=mask_new.get_rect()
+'''
 
 
 ticks_start=0
@@ -128,9 +184,21 @@ wait_time=0
 
 rot_counter=0
 rotation = .1
-
 map_counter = 0
 
+# transparency mask dummied out code
+'''
+print "begin test"
+masked = maps[map_counter][0].copy().convert_alpha()
+masked.blit(mask, (0, 0), None, pygame.BLEND_RGBA_MULT)
+screen.blit(masked, (0, 0))
+
+#screen.blit(mask_new,mask_new_rect)
+#dummied out code to check that the mask created correctly
+
+'''
+
+print "Running"
 while 1:
   #main loop
 
